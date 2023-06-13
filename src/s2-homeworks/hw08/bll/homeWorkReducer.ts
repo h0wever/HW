1267369dc25fa69a521dafa09d43c681d358f1b1
Siarhei <wkmcvsm@gmail.com>
2023-06-13 13:23:53 +0300

    fix: HW8.tsx

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,11 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            if (action.payload === 'up') {
+                state.sort((a, b) => a.name.localeCompare(b.name))
+            } else if (action.payload === 'down') {
+                state.sort((a, b) => a.name.localeCompare(b.name)).reverse()
+            }
             return state // need to fix
         }
         case 'check': {
